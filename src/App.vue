e166a22be0a1ea8275ddcf33cd73c319a99c6d7f
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-25 13:30:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav class="col-12">
+
+    </nav>
+  </div>
+</template>
+
+<script>
+
+// import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: sans-serif;
+  
+  nav{
+    background-color: #2E3A46;
+    height: 70px;
+  }
+}
+</style>
